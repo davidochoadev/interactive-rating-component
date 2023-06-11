0042e202030cf080b69263d6bd62fde867f956b8
davideochoaa <davideochoaa@gmail.com>
2023-06-11 12:13:07 +0200

    ✅ Feat: Created script insidevoting component

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,14 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		colors: {
+			orange: 'hsl(25, 97%, 53%)',
+			white: 'hsl(0, 0%, 100%)',
+			lightGray: 'hsl(217, 12%, 63%)',
+			mediumGray: 'hsl(216, 12%, 54%)',
+			darkBlue: 'hsl(213, 19%, 18%)',
+			veryDarkBlue: 'hsl(216, 12%, 8%)'
+		 },
 	},
 	plugins: [],
 }
