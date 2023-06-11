9a6203979c11f7c56268f0ff89d7253ac98bd95c
davideochoaa <davideochoaa@gmail.com>
2023-06-11 04:16:02 +0200

    ✅ Feat: Created first interactive component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-   site: 'https://davidochoadev.github.io',
-   base: '/interactive-rating-component',
+  site: 'https://davidochoadev.github.io',
+  base: '/interactive-rating-component',
+  integrations: [tailwind()]
 });
